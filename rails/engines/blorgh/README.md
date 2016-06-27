eaee05b9eedf2d17666e1e674ff49d5cb2e835b1
Tim Rosenblatt <github@timrosenblatt.com>
2016-06-26 21:32:17 -0700

    basic blog engine

---
+++ rails/engines/blorgh/README.md
@@ -0,0 +1,1 @@
+Just going through http://guides.rubyonrails.org/engines.html
---
+++ rails/engines/blorgh/config/routes.rb
@@ -0,0 +1,7 @@
+Blorgh::Engine.routes.draw do
+  resources :articles do
+    resources :comments
+  end
+
+  root to: 'articles#index'
+end
