334129a95596c727ad6abda1d386addb10e516ba
Tim Rosenblatt <tim.rosenblatt@futureadvisor.com>
2019-04-07 06:30:56 -0700

    Adds demo of package-scoped variable initialization

--- go/src/make_a_package/app/entry.go
+++ go/src/make_a_package/app/entry.go
@@ -5,7 +5,26 @@ import (
 	"make_a_package/greet"
 )
 
+/*
+This is wild. Package-scoped variables are declared over repeated
+initialization cycles. So, I can declare `a` which is dependent on
+`b` first, which is dependent on a function call, which itself is
+dependent on a variable `c` that is only declared on a later line.
+*/
+
+var (
+	a int = b
+	b int = f()
+	c int = 1
+)
+
+func f() int {
+	return c + 1
+}
+
 func main() {
+	fmt.Println(a, b, c)
+
 	fmt.Println("version is " + version)
 	fmt.Println(greet.Morning)
 }
