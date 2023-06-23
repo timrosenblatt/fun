621d5820bbffb1359391209e37e03fa5b3ad25c5
Tim Rosenblatt <github@timrosenblatt.com>
2023-06-22 18:32:38 -0700

    Adds velaux to the kubevela installer

--- kubevela/setup.sh
+++ kubevela/setup.sh
@@ -11,3 +11,14 @@ brew install kubevela
 helm repo add kubevela https://charts.kubevela.net/core
 helm repo update
 helm install --create-namespace -n vela-system kubevela kubevela/vela-core --wait
+
+# The above is the complete installation process for kubevela itself, but the tutorial expects
+# that VelaUX is installed too
+
+vela addon enable velaux
+
+echo "The default username is 'admin' and the password is 'VelaUX12345'"
+
+# Just using the most basic way to access velaux, but for a more professional
+# installation they support using Services.
+vela port-forward addon-velaux -n vela-system 8000:8000
