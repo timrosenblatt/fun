196ebfdecb594fa2dea0896974820f2c1952b649
Tim Rosenblatt <github@timrosenblatt.com>
2023-08-30 13:44:13 -0700

    temp commit for debugging
    turns out aws_subnet_ids is deprecated, and aws_subnets is the new hotness. But,
    I'm hitting save on this interim work in case the semantics have changed and
    i need to do a bunch of rewriting

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,30 +8,136 @@ variable "server_port" {
     default = 8080
 }
 
-# This makes it easy to do 
-# curl http://$(tf output -raw public_ip):8080
-output "public_ip" {
-    value = aws_instance.example.public_ip
-    description = "The public IP address for the server"
+# # This makes it easy to do 
+# # curl http://$(tf output -raw public_ip):8080
+# output "public_ip" {
+#     value = aws_instance.example.public_ip
+#     description = "The public IP address for the server"
+# }
+
+output "alb_dns_name" {
+    value = aws_lb.example.dns_name
+    description = "The domain name of the load balancer"
+}
+
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnet_ids" "default" {
+    vpc_id = data.aws_vpc.default.id
 }
 
 
-resource "aws_instance" "example" {
-    ami = "ami-0c55b159cbfafe1f0"  # `aws ec2 describe-images --image-ids ami-0c55b159cbfafe1f0` says this is a safe image
-    instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
-    key_name = aws_key_pair.my_key_pair.key_name
+resource "aws_lb" "example" {
+    name = "terraform-asg-example"
+    load_balancer_type = "application"
+    subnets = data.aws_subnet_ids.default.ids
+    security_groups = [aws_security_group.alb.id]
+}
 
+resource "aws_lb_listener" "http" {
+    load_balancer_arn = aws_lb.example.load_balancer_arn
+    port = 80
+    protocol = "HTTP"
+
+    default_action {
+        type = "fixed-response"
+
+        fixed_response {
+            content_type = "text/plain"
+            message_body = "404: page not found"
+            status_code = 404
+        }
+    }
+}
+
+resource "aws_lb_target_group" "asg" {
+    name = "terraform-asg-example"
+    port = var.server_port
+    protocol = "HTTP"
+    vpc_id = data.aws_vpc.default.id
+
+    health_check {
+        path = "/"
+        protocol = "HTTP"
+        matcher = "200"
+        interval = 15
+        timeout = 3
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+    }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+    listener_arn = aws_lb_listener.http.arn
+    priority = 100
+
+    condition {
+        path_pattern {
+            values = ["*"]
+        }
+    }
+
+    action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.asg.arn
+    }
+}
+
+resource "aws_security_group" "alb" {
+    name = "terraform-example-alb"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_launch_configuration" "example" {
+    image_id = "ami-0c55b159cbfafe1f0"  # `aws ec2 describe-images --image-ids ami-0c55b159cbfafe1f0` says this is a safe image
+    instance_type = "t2.micro"
+    security_groups = [aws_security_group.instance.id]
+    key_name = aws_key_pair.my_key_pair.key_name
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
                 nohup busybox httpd -f -p ${var.server_port} &
                 EOF
-
-    tags = {
-        Name = "my instance"
+    
+    lifecycle {
+        create_before_destroy = true
     }
 }
+
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_configuration.example.name
+    vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+    target_group_arns = [aws_lb_target_group.asg.arn]
+    health_check_type = "ELB"
+    
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key = "Name"
+        value = "terraform-asg-example"
+        propagate_at_launch = true
+    }
+}
+
+
 
 resource "aws_key_pair" "my_key_pair" {
   key_name   = "tims_m2_mbp"
@@ -60,3 +166,4 @@ resource "aws_security_group" "instance" {
         cidr_blocks = ["73.231.224.243/32"]
     }
 }
+
